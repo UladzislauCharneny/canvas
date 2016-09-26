c08507cd13fc8b0545b2a5bbd7b28688a8e1b430
UladzislauCharneny <chernyony2015@gmail.com>
2016-09-26 23:34:49 +0300

    Code refactoring

--- canvas.rb
+++ canvas.rb
@@ -1,48 +1,48 @@
 class Canvas
-    attr_accessor :canvas, :canvas_length, :canvas_height
+  attr_accessor :canvas, :canvas_length, :canvas_height
 
-    def initialize(canvas_length, canvas_height)
-        @canvas_length = canvas_length
-        @canvas_height = canvas_height
-        @canvas = Array.new(canvas_length) { Array.new(canvas_height, 0) }
+  def initialize(canvas_length, canvas_height)
+    @canvas_length = canvas_length
+    @canvas_height = canvas_height
+    @canvas = Array.new(canvas_length) { Array.new(canvas_height, 0) }
+  end
+
+  def add_layer(num, position_top, position_left, layer_width, layer_height)
+    layer_height = check_height(position_top, layer_height)
+
+    layer_width = check_width(position_left, layer_width)
+
+    fill_in_canvas(num, position_top, position_left, layer_height, layer_width)
+  end
+
+  private
+
+  def check_height(position_top, layer_height)
+    layer_bottom_position = position_top + layer_height
+
+    if layer_bottom_position > canvas_length - 1
+      layer_height = canvas_length - position_top
     end
 
-    def add_layer(num, position_top, position_left, layer_width, layer_height)
-        layer_height = check_height(position_top, layer_height)
+    layer_height
+  end
 
-        layer_width = check_width(position_left, layer_width)
+  def check_width(position_left, layer_width)
+    layer_right_border_position = position_left + layer_width
 
-        fill_in_canvas(num, position_top, position_left, layer_height, layer_width)
+    if layer_right_border_position > canvas_height - 1
+      layer_width = canvas_height - position_left
     end
 
-    private
+    layer_width
+  end
 
-    def check_height(position_top, layer_height)
-      layer_bottom_position = position_top + layer_height
-
-      if layer_bottom_position > canvas_length - 1
-        layer_height = canvas_length - position_top
+  def fill_in_canvas(num, position_top, position_left, layer_height, layer_width)
+    position_top.upto(position_top+layer_height-1) do |main_index|
+      position_left.upto(position_left+layer_width-1) do |index|
+        canvas[main_index][index] = num
       end
-
-      layer_height
     end
-
-    def check_width(position_left, layer_width)
-      layer_right_border_position = position_left + layer_width
-
-      if layer_right_border_position > canvas_height - 1
-        layer_width = canvas_height - position_left
-      end
-
-      layer_width
-    end
-
-    def fill_in_canvas(num, position_top, position_left, layer_height, layer_width)
-      position_top.upto(position_top+layer_height-1) do |main_index|
-        position_left.upto(position_left+layer_width-1) do |index|
-          canvas[main_index][index] = num
-        end
-      end
-    end
+  end
 end
 
--- canvas_spec.rb
+++ canvas_spec.rb
@@ -1,27 +1,27 @@
 require './canvas.rb'
 
 RSpec.describe do
-    it 'create new canvas' do
-        canvas = Canvas.new(3, 3)
-        expect(canvas.canvas).to eq([[0,0,0], [0,0,0], [0,0,0]])
-    end
+  it 'create new canvas' do
+    canvas = Canvas.new(3, 3)
+    expect(canvas.canvas).to eq([[0,0,0], [0,0,0], [0,0,0]])
+  end
 
-    it 'add layer' do
-        canvas = Canvas.new(3, 3)
-        canvas.add_layer(1, 1, 1, 2, 2)
-        expect(canvas.canvas).to eq([[0,0,0], [0,1,1], [0,1,1]])
-    end
+  it 'add layer' do
+    canvas = Canvas.new(3, 3)
+    canvas.add_layer(1, 1, 1, 2, 2)
+    expect(canvas.canvas).to eq([[0,0,0], [0,1,1], [0,1,1]])
+  end
 
-    it 'add layer out of border' do
-        canvas = Canvas.new(3, 3)
-        canvas.add_layer(1, 1, 1, 4, 4)
-        expect(canvas.canvas).to eq([[0,0,0], [0,1,1], [0,1,1]])
-    end
+  it 'add layer out of border' do
+    canvas = Canvas.new(3, 3)
+    canvas.add_layer(1, 1, 1, 4, 4)
+    expect(canvas.canvas).to eq([[0,0,0], [0,1,1], [0,1,1]])
+  end
 
-    it 'add additional layer' do
-        canvas = Canvas.new(3, 3)
-        canvas.add_layer(2, 1, 1, 2, 2)
-        canvas.add_layer(1, 0, 0, 2, 2)
-        expect(canvas.canvas).to eq([[1,1,0], [1,1,2], [0,2,2]])
-    end
+  it 'add additional layer' do
+    canvas = Canvas.new(3, 3)
+    canvas.add_layer(2, 1, 1, 2, 2)
+    canvas.add_layer(1, 0, 0, 2, 2)
+    expect(canvas.canvas).to eq([[1,1,0], [1,1,2], [0,2,2]])
+  end
 end
